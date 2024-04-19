a738e1d731b77cfad47072fb8540e97fb4098916
coop3rock <coop3rocksdm@gmail.com>
2024-04-19 12:40:05 +0300

    Initiate project

---
+++ testGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Дмитро Солоп on 19.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
